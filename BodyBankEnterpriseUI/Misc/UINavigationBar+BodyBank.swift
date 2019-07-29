dce495f2223d726b68e17c0038ed6edc00be9cfb
Masashi Horita <masashi@bodygram.com>
2019-07-29 12:02:17 +0900

    🆕add color

--- BodyBankEnterpriseUI/Misc/UINavigationBar+BodyBank.swift
+++ BodyBankEnterpriseUI/Misc/UINavigationBar+BodyBank.swift
@@ -12,7 +12,9 @@ extension UINavigationBar{
     func setUpBodyBankGradient() -> UIImage?{
         let rect = CGRect(x: 0, y: -UIApplication.shared.statusBarFrame.height, width: UIApplication.shared.statusBarFrame.width, height: UIApplication.shared.statusBarFrame.height + frame.height)
         let layer = CAGradientLayer(frame: rect, colors: [
-            UIColor.BodyBank.Gradient.begin,
+            UIColor.BodyBank.Gradient.blue,
+            UIColor.BodyBank.Gradient.blue,
+//            UIColor.BodyBank.Gradient.begin,
             UIColor.BodyBank.Gradient.end
             ], startPoint: CGPoint(x: 0, y: 0.5), endPoint: CGPoint(x: 1, y: 0.5))
         if let image = layer.createGradientImage(){
